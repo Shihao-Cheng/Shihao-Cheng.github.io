47c9bf624c5c2441caec494099fa69375c48a2e1
Shihao-Cheng <91623576+Shihao-Cheng@users.noreply.github.com>
2026-06-19 07:39:29 +0000

    Deploying to gh-pages from @ Shihao-Cheng/Shihao-Cheng.github.io@57145378b3404a25325e07c03a89d3507a78937d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Selected project.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
