993a54f4077c53b29ee5980304812484ba1a8fd7
Shihao-Cheng <91623576+Shihao-Cheng@users.noreply.github.com>
2026-06-20 20:12:37 +0000

    Deploying to gh-pages from @ Shihao-Cheng/Shihao-Cheng.github.io@2c210e5b89f2028d64bf9a9548c687c9fa1ab241 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Shihao-Cheng.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://shihaocheng.com/#contact">contact us</a>.'
               }
             ]
           }
